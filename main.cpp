1e896f6f86d594650ead1eb575077b36685f2f1a
Dec <112513827+752986@users.noreply.github.com>
2023-10-26 15:45:04 -0600

    Almost there to multithreading

--- main.cpp
+++ main.cpp
@@ -12,12 +12,26 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
+#include <ctime>
 #include <complex>
+#include <vector>
+#include <thread>
 #include "colormap.cpp"
 using namespace std;
 
 int mandelbrot(complex<double> c, int max_iterations);
 double smooth_mandelbrot(complex<double> c, int max_iterations);
+void draw_mandelbrot(
+	vector<vector<double>> pixels,
+	double startX, 
+	double startY, 
+	double endX, 
+	double endY, 
+	int screenX, 
+	int screenY, 
+	int screenWidth
+);
+void draw_pixels(vector<vector<double>> pixels, int max_iterations);
 
 const int WIDTH = 1000;
 const int HEIGHT = 1000;
@@ -27,28 +41,95 @@ int main() {
 	al_init_primitives_addon();
 	auto display = al_create_display(WIDTH, HEIGHT);
 
-	complex<double> c;
-	double num;
-
 	double zoom = WIDTH / 4;
 
-	const int max_iterations = 80;
+	vector<vector<double>> pixels(WIDTH, vector<double>(HEIGHT, 0.0));
+
+	auto start_time = clock();
+
+	int num_threads = thread::hardware_concurrency();
+	// thread threads[num_threads];
+	for (int i = 0; i < num_threads; i++) {
+		int pixel_width = WIDTH / num_threads;
+		double unit_width = 4 / (double)num_threads;
+		// threads[i] = thread(
+		// 	draw_mandelbrot,
+		// 	pixels,
+		// 	-2 + (unit_width * i), 
+		// 	-2, 
+		// 	-2 + (unit_width * (i + 1)), 
+		// 	2, 
+		// 	pixel_width * i, 
+		// 	0, 
+		// 	pixel_width
+		// );
+		draw_mandelbrot(
+			pixels,
+			-2 + (unit_width * i), 
+			-2, 
+			-2 + (unit_width * (i + 1)), 
+			2, 
+			pixel_width * i, 
+			0, 
+			pixel_width
+		);
+	}
 
 	al_lock_bitmap(al_get_target_bitmap(), al_get_display_format(display), al_get_display_flags(display));
-	for (double x = -2; x < 2; x += 1 / zoom) {
-		for (double y = -2; y < 2; y += 1 / zoom) {
-			c = complex<double>(x, y);
-			num = smooth_mandelbrot(c, max_iterations);
-			const float* color = colormap::sample_colormap(colormap::MAGMA, (float)num / (float)max_iterations);
-			al_put_pixel(x * zoom + (WIDTH / 2), y * zoom + (HEIGHT / 2), al_map_rgb_f(color[0], color[1], color[2]));
-		}
-	}
+
+
+	draw_pixels(pixels, 80);
+	
+	// for (int i = 0; i < num_threads; i++) {
+	// 	threads[i].join();
+	// }
+
+	auto run_time = clock() - start_time;
+	printf("%d ms\n", run_time);
 
 	al_unlock_bitmap(al_get_target_bitmap());
 	al_flip_display();
 
 	system("pause");
 	al_destroy_display(display);
+}
+
+void draw_mandelbrot(
+	vector<vector<double>> pixels,
+	double startX, 
+	double startY, 
+	double endX, 
+	double endY, 
+	int screenX, 
+	int screenY, 
+	int screenWidth // screenHeight is inferred
+) {
+	complex<double> c;
+	double num;
+
+	int max_iterations = 80;
+
+	double scale = screenWidth / (endX - startX);
+
+	for (double x = startX; x < endX; x += 1 / scale) {
+		for (double y = startY; y < endY; y += 1 / scale) {
+			c = complex<double>(x, y);
+			num = smooth_mandelbrot(c, max_iterations);
+			int xCoord = x * scale + screenX - (scale * startX);
+			int yCoord = y * scale + screenY - (scale * startY);
+			pixels[xCoord][yCoord] = num; 
+		}
+	}
+}
+
+void draw_pixels(vector<vector<double>> pixels, int max_iterations) {
+	for (int x = 0; x < WIDTH; x++) {
+		for (int y = 0; y < HEIGHT; y++) {
+			double num = pixels[x][y];
+			const float* color = colormap::sample_colormap(colormap::MAGMA, num / (float)max_iterations);
+			al_put_pixel(x, y, al_map_rgb_f(color[0], color[1], color[2]));
+		}
+	}
 }
 
 int mandelbrot(complex<double> c, int max_iterations) {
