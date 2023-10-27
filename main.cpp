5ab094a04a17221a6a6a81e8c04b7891d7f46280
Dec <112513827+752986@users.noreply.github.com>
2023-10-27 15:20:11 -0600

    Multithreading finally works!

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 int mandelbrot(complex<double> c, int max_iterations);
 double smooth_mandelbrot(complex<double> c, int max_iterations);
 void draw_mandelbrot(
-	vector<vector<double>> pixels,
+	vector<vector<double>> &pixels,
 	double startX, 
 	double startY, 
 	double endX, 
@@ -31,7 +31,7 @@ void draw_mandelbrot(
 	int screenY, 
 	int screenWidth
 );
-void draw_pixels(vector<vector<double>> pixels, int max_iterations);
+void draw_pixels(vector<vector<double>> &pixels, int max_iterations);
 
 const int WIDTH = 1000;
 const int HEIGHT = 1000;
@@ -48,12 +48,22 @@ int main() {
 	auto start_time = clock();
 
 	int num_threads = thread::hardware_concurrency();
-	// thread threads[num_threads];
+	thread threads[num_threads];
 	for (int i = 0; i < num_threads; i++) {
 		int pixel_width = WIDTH / num_threads;
 		double unit_width = 4 / (double)num_threads;
-		// threads[i] = thread(
-		// 	draw_mandelbrot,
+		threads[i] = thread(
+			draw_mandelbrot,
+			ref(pixels), // `ref()` is *very* important here
+			-2 + (unit_width * i), 
+			-2, 
+			-2 + (unit_width * (i + 1)), 
+			2, 
+			pixel_width * i, 
+			0, 
+			pixel_width
+		);
+		// draw_mandelbrot(
 		// 	pixels,
 		// 	-2 + (unit_width * i), 
 		// 	-2, 
@@ -63,26 +73,15 @@ int main() {
 		// 	0, 
 		// 	pixel_width
 		// );
-		draw_mandelbrot(
-			pixels,
-			-2 + (unit_width * i), 
-			-2, 
-			-2 + (unit_width * (i + 1)), 
-			2, 
-			pixel_width * i, 
-			0, 
-			pixel_width
-		);
 	}
 
 	al_lock_bitmap(al_get_target_bitmap(), al_get_display_format(display), al_get_display_flags(display));
 
+	for (int i = 0; i < num_threads; i++) {
+		threads[i].join();
+	}
 
 	draw_pixels(pixels, 80);
-	
-	// for (int i = 0; i < num_threads; i++) {
-	// 	threads[i].join();
-	// }
 
 	auto run_time = clock() - start_time;
 	printf("%d ms\n", run_time);
@@ -95,7 +94,7 @@ int main() {
 }
 
 void draw_mandelbrot(
-	vector<vector<double>> pixels,
+	vector<vector<double>> &pixels,
 	double startX, 
 	double startY, 
 	double endX, 
@@ -122,7 +121,7 @@ void draw_mandelbrot(
 	}
 }
 
-void draw_pixels(vector<vector<double>> pixels, int max_iterations) {
+void draw_pixels(vector<vector<double>> &pixels, int max_iterations) {
 	for (int x = 0; x < WIDTH; x++) {
 		for (int y = 0; y < HEIGHT; y++) {
 			double num = pixels[x][y];
